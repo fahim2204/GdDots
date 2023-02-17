39b3bb466f1790b275f64cec390ceeaf9ced43af
fahim2204 <fahimfaisal1998@gmail.com>
2023-02-18 01:05:25 +0600

    Services Section Modified

--- pages/services.js
+++ pages/services.js
@@ -10,72 +10,72 @@ import Img6 from "../public/assets/6.png";
 const Services = () => {
   return (
     <div id="services" className="bg-pink-50">
-      <section className="text-gray-600">
-        <div className="px-8 py-12 mx-auto">
-          <div className="flex  mb-5 flex-col items-center text-center">
-            <h1 className="text-5xl mb-2 text-gray-900 uppercase tracking-wider">
+      <section className="max-w-6xl mx-auto px-5">
+        <div className="py-12 mx-auto">
+          <div className="flex mb-5 flex-col items-center text-center">
+            <h1 className="text-4xl sm:text-5xl mb-10 text-gray-900 uppercase tracking-wider font-sans">
               Our <span className="text-orange-600">Services</span>
             </h1>
           </div>
-          <div className=" justify-center text-center items-center mx-auto gap-16 grid sm:grid-cols-2 md:grid-cols-3">
-            <div class="w-full h-full p-6 text-center  mb-4 rounded-3xl drop-shadow-xl bg-white border border-gray-200 hover:bg-orange-600 text-gray-900 hover:text-white transition duration-300 ">
+          <div className="justify-center text-center items-center mx-auto gap-4 md:gap-8 lg:gap-16 grid sm:grid-cols-2 md:grid-cols-3">
+            <div class="h-full p-6 text-center mb-4 rounded-3xl drop-shadow-md bg-white border border-gray-200 hover:bg-orange-600 text-gray-900 hover:text-white cursor-pointer transition ease-in duration-300 ">
               <Image src={Img} className="object-fill object-center" alt=".." />
-              <h2 className="text-4xl  font-medium mb-2">
+              <h2 className="text-3xl md:text-4xl font-medium">
                 Graphics <br /> Design
               </h2>
             </div>
-            <div class="w-full h-full p-6 text-center  mb-4 rounded-3xl drop-shadow-xl bg-white border border-gray-200 hover:bg-orange-600 text-gray-900 hover:text-white transition duration-300">
+            <div class="h-full p-6 text-center mb-4 rounded-3xl drop-shadow-md bg-white border border-gray-200 hover:bg-orange-600 text-gray-900 hover:text-white cursor-pointer transition ease-in duration-300">
               <Image
                 src={Img2}
                 className="object-fill object-center mb-4"
                 alt=".."
               />
-              <h2 className="text-4xl  font-medium mb-2">
+              <h2 className="text-3xl md:text-4xl font-medium">
                 Motion <br /> Graphics
               </h2>
             </div>
 
-            <div class="w-full h-full p-6 text-center  mb-4 rounded-3xl drop-shadow-xl bg-white border border-gray-200 hover:bg-orange-600 text-gray-900 hover:text-white transition duration-300">
+            <div class="h-full p-6 text-center mb-4 rounded-3xl drop-shadow-md bg-white border border-gray-200 hover:bg-orange-600 text-gray-900 hover:text-white cursor-pointer transition ease-in duration-300">
               <Image
                 src={Img3}
                 className="object-fill object-center mb-2"
                 alt=".."
               />
-              <h2 className="text-4xl  font-medium mb-2">
+              <h2 className="text-3xl md:text-4xl font-medium">
                 3D <br /> Animation
               </h2>
             </div>
 
-            <div class="w-full h-full p-6 text-center  mb-4 rounded-3xl drop-shadow-xl bg-white border border-gray-200 hover:bg-orange-600 text-gray-900 hover:text-white transition duration-300">
+            <div class="h-full p-6 text-center mb-4 rounded-3xl drop-shadow-md bg-white border border-gray-200 hover:bg-orange-600 text-gray-900 hover:text-white cursor-pointer transition ease-in duration-300">
               <Image
                 src={Img4}
                 className="object-fill object-center mb-2"
                 alt=".."
               />
-              <h2 className="text-4xl  font-medium mb-2">
+              <h2 className="text-3xl md:text-4xl font-medium">
                 UI/UX <br /> Design
               </h2>
             </div>
 
-            <div class="w-full h-full p-6 text-center  mb-4 rounded-3xl drop-shadow-xl bg-white border border-gray-200 hover:bg-orange-600 text-gray-900 hover:text-white transition duration-300">
+            <div class="h-full p-6 text-center mb-4 rounded-3xl drop-shadow-md bg-white border border-gray-200 hover:bg-orange-600 text-gray-900 hover:text-white cursor-pointer transition ease-in duration-300">
               <Image
                 src={Img5}
                 className="object-fill object-center mb-2"
                 alt=".."
               />
-              <h2 className="text-4xl  font-medium mb-2">
+              <h2 className="text-3xl md:text-4xl font-medium">
                 Clipping
                 <br /> Path
               </h2>
             </div>
 
-            <div class="w-full h-full p-6 text-center  mb-4 rounded-3xl drop-shadow-xl bg-white border border-gray-200 hover:bg-orange-600 text-gray-900 hover:text-white transition duration-300">
+            <div class="h-full p-6 text-center mb-4 rounded-3xl drop-shadow-md bg-white border border-gray-200 hover:bg-orange-600 text-gray-900 hover:text-white cursor-pointer transition ease-in duration-300">
               <Image
                 src={Img6}
                 className="object-fill object-center mb-2"
                 alt=".."
               />
-              <h2 className="text-4xl  font-medium mb-2">
+              <h2 className="text-3xl md:text-4xl font-medium">
                 Video <br /> Editing
               </h2>
             </div>
