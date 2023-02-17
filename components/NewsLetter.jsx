a2486388807adf50627435fb401e105fcc16c0ca
fahim2204 <fahimfaisal1998@gmail.com>
2023-02-18 01:26:06 +0600

    Wrap Up

--- components/NewsLetter.jsx
+++ components/NewsLetter.jsx
@@ -1,27 +1,29 @@
 
 const NewsLetter = () => {
   return (
-    <div id="newsletter" className=" bg-gray-100 py-12">
-      <div className="mb-8 px-8">
-        <h3 className="text-4xl tracking-tight text-center">
-          Join Our Newsletter
-        </h3>
-      </div>
-      <form className=" flex justify-center px-8 md:px-56">
-        <div className="w-full ">
-          <input
-            type="email"
-            className="bg-white text-start border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full py-4 px-5"
-            placeholder="Submit Your Email Address"
-          />
+    <div id="newsletter" className="bg-gray-100 py-12">
+      <div className="max-w-6xl mx-auto px-5">
+        <div className="mb-8">
+          <h3 className="text-4xl tracking-tight text-center">
+            Join Our Newsletter
+          </h3>
         </div>
-        <button
-          type="button"
-          className="text-white bg-gradient-to-r from-orange-500 via-orange-500 to-orange-400 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300  font-medium rounded-lg text-sm px-5 py-0 text-center -ml-2"
-        >
-          Subscribe
-        </button>
-      </form>
+        <form className="flex justify-center">
+          <div className="max-w-3xl w-full">
+            <input
+              type="email"
+              className="bg-white text-start border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full py-4 px-5"
+              placeholder="Submit Your Email Address"
+            />
+          </div>
+          <button
+            type="button"
+            className="text-white hover:scale-105 bg-gradient-to-r from-orange-500 via-orange-500 to-orange-400 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300  font-medium rounded-lg text-sm px-5 py-0 text-center -ml-2  transition-all duration-300"
+          >
+            Subscribe
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
